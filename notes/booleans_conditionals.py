c59ed7435b0c8a52fb0aa4a516dbccfce1c674a2
Julia Nowak <146851541+KamiJN@users.noreply.github.com>
2025-07-01 20:32:39 +0200

    Update booleans_conditionals.py with new examples and exercises.

--- notes/booleans_conditionals.py
+++ notes/booleans_conditionals.py
@@ -72,3 +72,27 @@ is_workday = False
 
 actual = prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday)
 print(actual) # output: True
+
+# =============================================================================
+# 6. Instrukcja pass - zastępuje przyszły kod, po jej wywołaniu nic się nie dzieje - nie doprowadza ona do błędu.
+# Przydaje się, gdy zostanie zostawiona pusta pętla, warunek, itd. 
+# =============================================================================
+
+# =============================================================================
+# 7. Funkcje zwracające "True", według podanego opisu warunku.
+# =============================================================================
+def wants_all_toppings(ketchup, mustard, onion):
+    """Return whether the customer wants "the works" (all 3 toppings)
+    """
+    return ketchup and mustard and onion
+
+def wants_plain_hotdog(ketchup, mustard, onion):
+    """Return whether the customer wants a plain hot dog with no toppings.
+    """
+    return not (ketchup or mustard or onion) # / return not ketchup and not mustard and not onion
+
+def exactly_one_sauce(ketchup, mustard, onion):
+    """Return whether the customer wants either ketchup or mustard, but not both.
+    (You may be familiar with this operation under the name "exclusive or")
+    """
+    return (ketchup or mustard) and not (ketchup and mustard) # / return (ketchup and not mustard) or (mustard and not ketchup)
