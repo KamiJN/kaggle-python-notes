a47e7eacae3372372ed10401e5a70a5e7252193d
Julia Nowak <146851541+KamiJN@users.noreply.github.com>
2025-07-11 15:56:41 +0200

    Updating lists.py file with new topics.

--- notes/lists.py
+++ notes/lists.py
@@ -1,6 +1,6 @@
 """
-Kaggle Python Course - Days 6: Lists
-Notatki: Julia Nowak | Data: 04-07-2025
+Kaggle Python Course - Days 6 & 7: Lists
+Notatki: Julia Nowak | Data: 04-07-2025 & 05-07-2025
 """
 
 # =============================================================================
@@ -35,5 +35,35 @@ print(max(fib_seq)) # output: 8
 print(min(fib_seq)) # output: 1
 
 # =============================================================================
-# 3. Wprowadzenie do obiektów
+# 3. Przykładowe metody list
+# ============================================================================= 
+# list.append() - dodaje nowy element na koniec listy
+print(planets.append('Pluto')) # output: None, nic nie zwraca
+print(planets) # output: ['Mercury', 'Venus', 'unknown', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+# list.pop() -  usuwa i zwraca ostatni element listy
+print(planets.pop()) # output: Pluto
+print(planets) # ['Mercury', 'Venus', 'unknown', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+# list.index() - zwraca indeks podanego elementu, jeśli go nie ma, zwraca błąd
+print(planets.index('Saturn')) # output: 5
+# Aby uniknąć błędu, można skorzystać z operatora "in":
+if "Earth" in planets:
+    print(planets.index('Earth'))
+else:
+    print("Goodbye World!")
+
 # =============================================================================
+# 4. Krotki (ang. "Tuples")
+# ============================================================================= 
+# Różnią się od list głównie deklaracją i niemożnością ich późniejszej modyfikacji
+t1 = 'a', 'b', 'c'
+t2 = ('d', 'e', 'f')
+t3 = 11, 13, 17
+print(t1) # output: ('a', 'b', 'c')
+# t1[0] = 'A' # ERROR!
+
+# Używane są często przy funkcjach, zwracających więcej niż jedną wartość.
+# as_integer_ratio() - metoda obiektów typu 'float', która zwraca licznik i mianownik w formie krotki.
+x = 0.125
+print(x.as_integer_ratio()) # output: (1, 8)
+numerator, denominator = x.as_integer_ratio()
+print(numerator / denominator) # output: 0.125
