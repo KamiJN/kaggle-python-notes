b087c01300b52802330b04cb46ab47ac9d297b40
Julia Nowak <146851541+KamiJN@users.noreply.github.com>
2025-07-12 21:08:45 +0200

    Updating lists.py with solved exercises.

--- notes/lists.py
+++ notes/lists.py
@@ -1,6 +1,6 @@
 """
 Kaggle Python Course - Days 6 & 7: Lists
-Notatki: Julia Nowak | Data: 04-07-2025 & 05-07-2025
+Notatki: Julia Nowak | Data: 11-07-2025 & 12-07-2025
 """
 
 # =============================================================================
@@ -67,3 +67,55 @@ x = 0.125
 print(x.as_integer_ratio()) # output: (1, 8)
 numerator, denominator = x.as_integer_ratio()
 print(numerator / denominator) # output: 0.125
+
+# =============================================================================
+# 5. Zadania - rozwiązania
+# ============================================================================= 
+# 1)
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    if len(L) < 2:
+        return None
+    return L[1]
+
+# 2)
+def losing_team_captain(teams):
+    """Given a list of teams, where each team is a list of names, return the 2nd player (captain)
+    from the last listed team
+    """
+    return teams[-1][1] # / return teams[len(teams) - 1][1]
+
+# 3)
+def purple_shell(racers):
+    """Given a list of racers, set the first place racer (at the front of the list) to last
+    place and vice versa.
+    
+    >>> r = ["Mario", "Bowser", "Luigi"]
+    >>> purple_shell(r)
+    >>> r
+    ["Luigi", "Bowser", "Mario"]
+    """
+    racers[0], racers[-1] = racers[-1], racers[0] 
+
+# 4)
+a = [1, 2, 3]
+b = [1, [2, 3]]
+c = []
+d = [1, 2, 3][1:]
+
+# Put your predictions in the list below. Lengths should contain 4 numbers, the
+# first being the length of a, the second being the length of b and so on.
+lengths = [3, 2, 0, 2]
+
+# 5)
+# party_attendees = ['Adela', 'Fleda', 'Owen', 'May', 'Mona', 'Gilbert', 'Ford']
+# A guest is considered 'fashionably late' if they arrived after at least half of the party's guests. However, they must not be the very last guest (that's taking it too far). In the above example, Mona and Gilbert are the only guests who were fashionably late.
+# Complete the function below which takes a list of party attendees as well as a person, and tells us whether that person is fashionably late.
+def fashionably_late(arrivals, name):
+    """Given an ordered list of arrivals to the party and a name, return whether the guest with that
+    name was fashionably late.
+    """
+    order = arrivals.index(name)
+    return order >= len(arrivals) / 2 and order != len(arrivals) - 1
