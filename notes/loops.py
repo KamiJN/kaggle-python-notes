d52ec49b0fa992953829bca86ac76792f851d077
Julia Nowak <146851541+KamiJN@users.noreply.github.com>
2025-07-24 18:45:03 +0200

    Updating loops.py with new completed exercises.

--- notes/loops.py
+++ notes/loops.py
@@ -114,3 +114,44 @@ any(iterable, /)
 '''
 
 # 2)
+def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is 
+    True if L[i] is greater than thresh, and False otherwise.
+    
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+    tmp = []
+    for el in L:
+        tmp.append(el > thresh)
+    return tmp
+# def elementwise_greater_than(L, thresh):
+#    return [el > thresh for el in L]
+
+# 3)
+def menu_is_boring(meals):
+    """Given a list of meals served over some period of time, return True if the
+    same meal has ever been served two days in a row, and False otherwise.
+    """
+    for i in range(len(meals) - 1):
+        if meals[i] == meals[i + 1]:
+            return True
+    return False
+
+# 4)
+'''
+def estimate_average_slot_payout(n_runs):
+    """Run the slot machine n_runs times and return the average net profit per run.
+    Example calls (note that return value is nondeterministic!):
+    >>> estimate_average_slot_payout(1)
+    -1
+    >>> estimate_average_slot_payout(1)
+    0.5
+    """
+    def estimate_average_slot_payout(n_runs):
+    # Zagraj na maszynie n_runs razy i wygeneruj wypłatę za każdą próbę.
+    payouts = [play_slot_machine()-1 for i in range(n_runs)]
+    # Oblicz średnią wszystkich wypłat.
+    avg_payout = sum(payouts) / n_runs
+    return avg_payout
+'''
