81ca10f159232a000c0f64bb9cab4a86bbc79339
Julia Nowak <146851541+KamiJN@users.noreply.github.com>
2025-07-28 18:57:57 +0200

    Uploading notes on the strings and dictionaries.

---
+++ notes/strings_dictionaries.py
@@ -0,0 +1,35 @@
+"""
+Kaggle Python Course - Days 10 & 11: Strings and Dictionaries
+Notatki: Julia Nowak | Data: 28-07-2025 & xx-0x-2025
+"""
+
+# =============================================================================
+# 1. String - składnia
+# =============================================================================
+example1 = 'What\'s up?'
+print(example1) # output: What's up?
+example2 = "That's \"cool\""
+print(example2) # output: That's "cool"
+example3 = "Look, a mountain: /\\" 
+print(example3) # output: Look, a mountain: /\
+
+print("Hello")
+print("World!")
+# output:
+# Hello
+# World!
+print("Hello", end='')
+print("World!", end='')
+# HelloWorld!
+
+# =============================================================================
+# 2. String - składnia
+# =============================================================================
+
+
+
+
+# =============================================================================
+# x. Zadania - rozwiązania
+# ============================================================================= 
+# 1)
